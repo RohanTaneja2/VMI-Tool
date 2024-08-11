8cfb483ab6191a8061e0547e9c206b64609e35ec
RohanTaneja2 <125910905+RohanTaneja2@users.noreply.github.com>
2024-08-11 18:05:46 -0500

    Update config.py to include Email Config for sysalert.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+# config.py
+
 class Config:
     DOMAIN_NAME = "example_vm"
     LOG_FILE = "vm_monitor.log"
-    ALERT_THRESHOLD = 5  # Example: threshold for alerts
-    MONITOR_INTERVAL = 5  # Interval in seconds between checks
+    ALERT_THRESHOLD = 5
+    MONITOR_INTERVAL = 10
+
+    # Email Configuration
+    EMAIL_SENDER = "your_email@example.com"
+    EMAIL_PASSWORD = "your_email_password"
+    SMTP_SERVER = "smtp.example.com"
+    SMTP_PORT = 587
